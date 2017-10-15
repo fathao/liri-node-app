0ee2861bbe15c5ccacc97049e3ad3140e8b5858f
= <=>
2017-10-14 18:05:26 -0700

    twitter done

--- key.js
+++ key.js
@@ -3,8 +3,11 @@ console.log('this is loaded');
 var twitterKeys = {
   consumer_key: '3Tl5fKERycDWM4aPNYmaMTGnR',
   consumer_secret: 'a678gZarLOMVdOyyunjInbsQgfoLrR7pWhML9UCR0VLFs8U6D2',
-  access_token_key: '	919322465905471488-xYJfK4kL9RhNLfR9CmKHFPTskC5awWP',
-  access_token_secret: 'aD33Dz974dHvkLkANaIxKWajhna9x9SmfHueGQN2wWgxN',
-}
+  access_token_key: '919322465905471488-xYJfK4kL9RhNLfR9CmKHFPTskC5awWP',
+  access_token_secret: 'aD33Dz974dHvkLkANaIxKWajhna9x9SmfHueGQN2wWgxN'
+};
 
-module.exports = twitterKeys;
+module.exports = {
+  twitter: twitterKeys,
+  spotify: 'xxxx'
+};
--- liri.js
+++ liri.js
@@ -1,2 +1,32 @@
+var Twitter = require('twitter');
+var keys = require('./key');
+
+
 var input = process.argv[2];
+
+
+
+if (input === "my-tweets") {
+  var client = new Twitter(keys.twitter);
+  var params = {
+    screen_name: 'realDonaldTrump',
+    count: 20
+  };
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    tweets.forEach(function(tweet, index) {
+      console.log((index + 1) + ':', tweet.text);
+    });
+
+    // var array = [6, 9, 1, 3, 5];
+    // for (var index = 0; index < array.length; index++) {
+    //   var number = array[index];
+    //   console.log(number, index);
+    // }
+
+    // [6, 9, 1, 3, 5].forEach(function(number, index) {
+    //   console.log(number, index);
+    // });
+  });
+
+}
 
