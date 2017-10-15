81fa5439eee0255f3d906f3a639bbd7eb552f868
= <=>
2017-10-14 20:39:47 -0700

    finished spotify

--- key.js
+++ key.js
@@ -6,8 +6,14 @@ var twitterKeys = {
   access_token_key: '919322465905471488-xYJfK4kL9RhNLfR9CmKHFPTskC5awWP',
   access_token_secret: 'aD33Dz974dHvkLkANaIxKWajhna9x9SmfHueGQN2wWgxN'
 };
+var spotifyKeys = {
+  id: '86018dfaceec49d78a0f7aa808f8803e',
+  secret: 'b299813078704139933e14f02cf8d9fa'
+};
+var omdbKey = '40e9cece';
 
 module.exports = {
   twitter: twitterKeys,
-  spotify: 'xxxx'
+  spotify: spotifyKeys,
+  omdb: omdbKey
 };
--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require("request");
 var keys = require('./key');
 
 
 var input = process.argv[2];
-
 
 
 if (input === "my-tweets") {
@@ -12,8 +13,8 @@ if (input === "my-tweets") {
     screen_name: 'realDonaldTrump',
     count: 20
   };
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    tweets.forEach(function(tweet, index) {
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    tweets.forEach(function (tweet, index) {
       console.log((index + 1) + ':', tweet.text);
     });
 
@@ -27,6 +28,68 @@ if (input === "my-tweets") {
     //   console.log(number, index);
     // });
   });
+} else if (input === "spotify-this-song") {
+  var client = new Spotify(keys.spotify); //put in local 
+  var songName = process.argv[3] || 'The Sign';
 
+  client.search({
+    type: 'track',
+    query: songName
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    // for (var i = 0; i < data.tracks.items.length; i++) {
+    //   if (data.tracks.items[i].name === "The sign") {
+    //     trackInfo = data.tracks.items[i];
+    //     break;
+    //   }
+
+    // }
+    //   trackInfo = trackInfo || data.tracks.items[0];
+
+    var trackInfo = data.tracks.items.filter(function (item) {
+      return item.name === songName;
+    })[0] || data.tracks.items[0];
+
+    var artists = trackInfo.artists.map(function (artist) {
+      return artist.name;
+    }).join(', ');
+    // var artistNames = [];
+    // for (var i = 0; i < artists.length; i++) {
+    //   var artist = artists[i];
+    //   artistNames.push(artist);
+    // }
+    // var artistsNamesString = artistNames.join(', ');
+    var album = trackInfo.album.name;
+    var name = trackInfo.name;
+    var url = trackInfo.preview_url || 'No preview url';
+    console.log("Artists:", artists);
+    console.log("Song name: " + name);
+    console.log("Preview Link: " + url);
+    console.log("Album Name: " + album);
+    //var artists = 
+    //console.log(data.tracks.items[0].artists[0].name); 
+
+    // console.log(JSON.stringify(data));
+  });
+} else if (input === "movie-this") {
+  request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + keys.omdb, function (error, response, body) {
+
+      // If there were no errors and the response code was 200 (i.e. the request was successful)...
+      if (!error && response.statusCode === 200) {
+
+        // Then we print out the imdbRating
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        var nodeArgs = process.argv;
+        var movieName = "";
+        for (var i = 2; i < nodeArgs.length; i++) {
+          if (i > 2 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+          } else {
+            movieName += nodeArgs[i];
+          }
+        }
+      }
+  });
 }
-
